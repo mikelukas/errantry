064f9036126cfc2c7d795e69a653789d60826349
mlukas <mike.lukas@gmail.com>
2016-03-18 16:41:30 -0400

    GameMode refactor: Created IntroMode to handle displaying of title and instructions, and transition to OverworldMode.

---
+++ src/intromode.cpp
@@ -0,0 +1,74 @@
+/*
+ * intromode.cpp
+ *
+ *  Created on: Mar 18, 2016
+ *      Author: mlukas
+ */
+
+#include "intromode.h"
+
+IntroMode::IntroMode(GameData& gameData, GameState& gameState)
+	: GameMode(gameData, gameState)
+{
+
+}
+
+void IntroMode::run()
+{
+	char answer, start;
+	do
+	{
+		cout<<"#/////**/----/**/////**/\**\\\\\**\----\**\\\\\#"<<endl
+			<<"|                                              |"<<endl
+			<<"|                                              |"<<endl
+			<<"|                  Errantry                    |"<<endl
+			<<"|                by Mike Lukas                 |"<<endl
+			<<"|                                              |"<<endl
+			<<"|                                              |"<<endl
+			<<"#\\\\\**\----\**\\\\\**\/**/////**/----/**/////#"<<endl
+			<<"Would you like to view the instructions?(y/n) ";
+		cin>>answer;
+	}while(!validateChoice(answer));
+
+	if(answer == 'Y' || answer == 'y')
+		{
+			cout<<"The world you have just set foot in has become one of chaos."<<endl
+				<<"Monsters ravish the land destroying all that once was beautiful"
+		  <<endl<<"to the people.  An errant warrior from another continent, it is"<<endl
+				<<"your job to defeat these monsters and their evil controllers"<<endl
+				<<"hiding in various caves around the continent.  Only after all"<<endl
+				<<"7 have been defeated, can the monsters be vanquished and peace"<<endl
+				<<"be restored."<<endl
+				<<"Your position on the continent is marked by an X on the map."<<endl
+				<<"You begin on the right side.  As you progress to the other"<<endl
+				<<"side of the continent, beware, for the monsters become stronger."<<endl
+				<<"The various C's along the map mark the entrances to caves."<<endl
+				<<"By moving to the caves, you can fight the evil controllers" <<endl
+				<<"of the monsters.  While you move around the map, you may encouner"<<endl
+				<<"monsters.  By defeating these monsters, you can increase your"<<endl
+				<<"experience points.  When you gain enough, you will advance 1"<<endl
+				<<"level.  Every level advance allows you to increase your attributes"<<endl
+				<<"and become stronger.  If you are defeated by a monster in battle,"<<endl
+				<<"you lose the game.  You may view your statistics at any time"<<endl
+				<<"by choosing that option on the map menu.  Build your levels"<<endl
+				<<"so that you are stronger and can fight monsters and controllers"<<endl
+				<<"better!  Good luck and enjoy Errantry!"<<endl;
+		}
+	cout<<"Type x and press enter to start the game:  ";
+	cin>>start;
+	cout<<"****************************************************"<<endl;
+
+	gameState.enterMode(overworld);
+}
+
+bool IntroMode::validateChoice(char answer)
+{
+	//postcondition:  determines if response entered by user is
+	//a valid response character and returns true if so, false
+	//if not.
+	if(answer == 'Y' || answer == 'y' || answer == 'n' || answer == 'N')
+		return true;
+	else
+		cout<<"Invalid Response"<<endl;
+	return false;
+}
---
+++ src/intromode.h
@@ -0,0 +1,27 @@
+#ifndef SRC_INTROMODE_H_
+#define SRC_INTROMODE_H_
+
+#include <iostream>
+#include "gamemode.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+/* Simple mode run at the start of that game that displays the title, and optionally
+ * instructions, then advances to OverworldMode.
+ */
+class IntroMode : public GameMode
+{
+	protected:
+
+		bool validateChoice(char answer);
+
+	public:
+		IntroMode(GameData&, GameState& gameState);
+		~IntroMode() { };
+
+		void run();
+};
+
+#endif
